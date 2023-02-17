16904c82e681a9d8acf573e811015399d18fe2f2
Mainagithubrit <njorogefrancismaina@gmail.com>
2023-02-17 10:37:04 -0800

    5-print_numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - prints all single numberss from base 10 to 0
+ * Return: 0
+ */
+int main(void)
+{
+int x;
+for (x = 0; x < 10; x++)
+printf("%d", x);
+putchar('\n');
+return (0);
+}
