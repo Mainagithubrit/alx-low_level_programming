a10c91c79c07ac6ace8cfb5f5659699c6931974f
Mainagithubrit <njorogefrancismaina@gmail.com>
2023-02-17 07:55:10 -0800

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - This program print the alphabetical order in lower case
+ * Return: 0
+ */
+int main(void)
+{
+	char ch = 'a';
+
+while(ch <= 'z') 
+{
+putchar("%c\n", ch);
+ch++
+}
+return (0);
+}
