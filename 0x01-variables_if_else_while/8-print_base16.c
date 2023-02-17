f39de5c761319d825f70122e47fde27acf50b8f8
Mainagithubrit <njorogefrancismaina@gmail.com>
2023-02-17 11:33:26 -0800

    8-print_base16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main -  a program that prints all the numbers of base 16 in lowercase
+ * Return: 0
+ */
+int main(void)
+{
+char ch;
+int x;
+
+ch = 'a';
+x = 0;
+while 
+(x < 10)
+putchar(x + '0');
+x++;
+while 
+(ch <= 'f')
+{
+putchar(ch);
+ch++;
+}
+putchar('\n');
+return (0);
+}
+
