f8504099cfabeacb70b1cfc95dfbde2b7a938522
Mainagithubrit <njorogefrancismaina@gmail.com>
2023-02-21 10:47:24 -0800

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -9,11 +9,11 @@ void print_alphabet_x10(void)
 char letter;
 int i;
 
-for (letter = 'a'; letter <= 'z' ; letter++)
+for (i = 0; i <= 10 ; i++)
 {
 _putchar(letter);
 
-for (i = 0; i <= 10; i++)
+for (letter = 'a'; letter <= 'z'; letter++)
 {
 _putchar('\n');
 }
