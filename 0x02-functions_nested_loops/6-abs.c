536bad2f4f01d3bd12c6e6c4fc9adf6ca71390a7
Mainagithubrit <njorogefrancismaina@gmail.com>
2023-02-21 12:02:19 -0800

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+
+/**
+ * _abs - compute the value of an integer
+ * @x: The integer
+ * Return: The absolute value
+ */
+int _abs(int x)
+{
+if (x >= 0)
+return (x);
+else
+return (-x);
+}
