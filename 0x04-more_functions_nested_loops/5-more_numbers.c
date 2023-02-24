b7296f1dca29fb30966a07e7274fb02d019b012d
Mainagithubrit <njorogefrancismaina@gmail.com>
2023-02-24 03:19:52 -0800

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,9 +9,9 @@ void more_numbers(void)
 {
 int x;
 int y;
-for (x = '1'; x <= 10; x++)
+for (x = 1; x <= 10; x++)
 {
-for (y = '0'; y <= 14; y++)
+for (y = 0; y <= 14; y++)
 {
 if (y >= 10)
 _putchar('1');
