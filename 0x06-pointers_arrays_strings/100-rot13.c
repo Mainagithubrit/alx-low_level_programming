f95fa4673554b99c1c8a8aeab94201367b246275
Mainagithubrit <njorogefrancismaina@gmail.com>
2023-03-02 01:09:25 -0800

    100-rot13.c

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * rot13 - a function that encodes a string using rot13.
+ * @n: pointer
+ * Return: 0
+ */
+char *rot13(char *n)
+{
+int i;
+int j;
+char data1[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+char datarot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+for (i = 0; n[i] != '0'; i++)
+{
+for (j = 0; j < 52; j++)
+{
+if (n[i] == data1[j])
+{
+n[i] = datarot[j];
+break;
+}
+}
+}
+return (n);
+}
