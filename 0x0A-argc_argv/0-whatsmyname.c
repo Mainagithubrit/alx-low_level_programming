a37f8e1ddbbe63ab134bcf076b9eecc354e99508
Mainagithubrit <njorogefrancismaina@gmail.com>
2023-03-10 02:25:23 -0800

    1-args.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,10 +1,10 @@
-#include "main.h"
 #include <stdio.h>
 /**
- * main - a program that prints its name, followed by a new line
+ * main - prints the name of the program
  * @argc: number of arguments
- * @argv: array containing elements
- * Retun: 0
+ * @argv: array of arguments
+ *
+ * Return: Always 0 (success)
  */
 int main(int argc, char **argv)
 {
