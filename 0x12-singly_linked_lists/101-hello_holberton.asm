74434f027ee716d97fdb505cfbd01724b82b3257
Mainagithubrit <njorogefrancismaina@gmail.com>
2023-03-30 05:35:29 -0700

    101-hello_holberton.asm

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,21 @@
+exterm printf
+
+section .text
+global main
+
+main:
+push rbp
+
+mov rdi,fnt
+mov rsi,msg
+mov rax,0
+call printf
+
+pop rbp
+
+mov rax,0
+ret
+
+section .data
+msg: db "Hello, Holberton", 0
+fmt: db "%s", 10, 0
