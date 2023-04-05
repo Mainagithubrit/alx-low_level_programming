85988f62537c118af1343f510cf4d2b8d5efa4f7
ghdhhdhdb <hhjjdn>
2023-04-05 00:48:37 -0700

    101-print_listint_safe.c

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,70 @@
+#include "lists.h"
+#include <stdio.h>
+size_t loop_listint_length(const listint_t *head);
+size_t print_listint_safe(const listint_t *head);
+
+/**
+ * loop_listint_length - counts the number of nodes
+ * @head: Pointer to the head of listint_t
+ * Return: 0
+ */
+size_t loop_listint_length(const listint_t *head)
+{
+const listint_t *first, *last;
+size_t nodes = 1;
+if (head == NULL || head->next == NULL)
+return (0);
+first = head->next;
+last = (head->next)->next;
+while (last)
+{
+if (first == last)
+{
+first = head;
+while (first != last)
+{
+nodes++;
+first = first->next;
+last = last->next;
+}
+first = first->next;
+while (first != last)
+{
+nodes++;
+first = first->next;
+}
+return (nodes);
+}
+first = first->next;
+last = (last->next)->next;
+}
+return (0);
+}
+/**
+ * print_listint_safe - a function that prints a listint_t linked list.
+ * @head: Pointer to the head
+ * Return: 0
+ */
+size_t print_listint_safe(const listint_t *head)
+{
+size_t nodes, index = 0;
+nodes = loop_listint_length(head);
+if (nodes == 0)
+{
+for (; head != NULL; nodes++)
+{
+printf("[%p] %d\n", (void *)head, head->n);
+head =  head->next;
+}
+}
+else
+{
+for (index = 0; index < nodes; index++)
+{
+printf("[%p] %d\n", (void *)head, head->n);
+head = head->next;
+}
+printf("-> [%p] %d\n", (void *)head, head->n);
+}
+return (nodes);
+}
