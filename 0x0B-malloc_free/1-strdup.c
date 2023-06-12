f2998fc2cf9dce694670eb101cc9abfdbf5a999a
Mainagithubrit <njorogefrancismaina@gmail.com>
2023-06-12 07:04:53 -0700

    1-strdup.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -13,16 +13,13 @@ char *_strdup(char *str)
 {
 int size, i;
 char *ptr;
-
 if (str == NULL)
 return (NULL);
-
 for (size = 0; str[size] != '\0'; size++)
 ptr = (char *)malloc((size + 1) * sizeof(char));
 
 if (ptr == NULL)
 return (NULL);
-
 for (i = 0; str[i] != '\0'; i++)
 ptr[i] = str[i];
 ptr[i] = '\0';
