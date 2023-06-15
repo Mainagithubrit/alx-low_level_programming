82ef54262c2b0199e3d379f0ab05c4de88eecc8c
Mainagithubrit <njorogefrancismaina@gmail.com>
2023-06-15 05:40:44 -0700

    1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -12,38 +12,34 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 char *p;
-unsigned int size1 = 0, size2 = 0, i;
+unsigned int size1 = 0, size2 = 0, i = 0, j = 0;
 
-if (s1 == NULL)
-s1 = " ";
-
-if (s2 == NULL)
-s2 = " ";
-
-while (s1[size1] != '\0')
-{
+while (s1 && s1[size1])
 size1++;
-}
 
-while (s2[size2] != '\0')
-{
+while (s2 && s2[size2])
 size2++;
-}
-if (n > size2)
-n = size2;
-p = malloc((size1 + n + 1) * sizeof(char));
 
-if (p == NULL)
-return (0);
+if (n < size2)
 
-for (i = 0; i < size1; i++)
+p = malloc(sizeof(char) * (size1 + n + 1));
+
+else
+
+p = malloc(sizeof(char) * (size1 + size2 + 1));
+if (!p)
+return (NULL);
+
+while (i < size1)
 {
 p[i] = s1[i];
+i++;
 }
-for (; i < (size1 + n); i++)
-{
-p[i] = s2[i - size1];
-}
+while (n < size2 && i < (size1 + n))
+p[i++] = s2[j++];
+
+while (n >= size2 && i < (size1 + size2))
+p[i++] = s2[j++];
 p[i] = '\0';
 
 return (p);
