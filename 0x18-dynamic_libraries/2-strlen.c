e50423d2091b798e9cd86f154750655fe16fddfa
Mainagithubrit <njorogefrancismaina@gmail.com>
2023-08-21 13:04:58 +0000

    Task 0

---
+++ 0x18-dynamic_libraries/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _strlen - a function that returns the length of a string.
+ * @s: character
+ * Return: 0;
+ */
+int _strlen(char *s)
+{
+int length = 0;
+while (*s != '\0')
+{
+length++;
+s++;
+}
+return (length);
+}
